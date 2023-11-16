41f909c587ce90a454b7647b6469bf7be0c23757
Antonette Caldwell <134739862+nebula-aac@users.noreply.github.com>
2023-11-16 17:28:22 -0600

    chore(repo): revert to previous script

--- version-packages.sh
+++ version-packages.sh
@@ -3,15 +3,15 @@
 # Usage: version-packages.sh
 # Example: version-packages.sh
 
-# Get the list of changed packages using Git history
-CHANGED_PACKAGES=$(git diff --name-only HEAD~1 HEAD | grep 'packages/.*/package.json' | sed 's/packages\/\(.*\)\/package.json/\1/')
+# Get the list of changed packages using Lerna
+CHANGED_PACKAGES=$(yarn lerna changed --json | jq -r '.[].name')
 
 if [ -n "$CHANGED_PACKAGES" ]; then
   echo "Changed packages detected: $CHANGED_PACKAGES"
-  IFS=', ' read -r -a PACKAGES <<< "$CHANGED_PACKAGES"
-  for PACKAGE_NAME in "${PACKAGES[@]}"; do
-    echo "Versioning package: $PACKAGE_NAME"
-    yarn lerna version --no-private --conventional-commits --conventional-prerelease --include-merged-tags --no-git-tag-version
+  yarn lerna version --no-private --conventional-commits --conventional-prerelease --include-merged-tags --no-git-tag-version
+
+  # Stage changes to package.json files
+  for PACKAGE_NAME in $CHANGED_PACKAGES; do
     grep -q "\"name\": \"$PACKAGE_NAME\"" packages/$PACKAGE_NAME/package.json && git add packages/$PACKAGE_NAME/package.json
   done
 else
