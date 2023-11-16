711dee7702cdbb1bef6a91c105b5c1ed58a57331
Antonette Caldwell <134739862+nebula-aac@users.noreply.github.com>
2023-11-16 17:31:56 -0600

    chore(repo): remove grepping package name

--- version-packages.sh
+++ version-packages.sh
@@ -8,12 +8,7 @@ CHANGED_PACKAGES=$(yarn lerna changed --json | jq -r '.[].name')
 
 if [ -n "$CHANGED_PACKAGES" ]; then
   echo "Changed packages detected: $CHANGED_PACKAGES"
-  yarn lerna version --no-private --conventional-commits --conventional-prerelease --include-merged-tags --no-git-tag-version
-
-  # Stage changes to package.json files
-  for PACKAGE_NAME in $CHANGED_PACKAGES; do
-    grep -q "\"name\": \"$PACKAGE_NAME\"" packages/$PACKAGE_NAME/package.json && git add packages/$PACKAGE_NAME/package.json
-  done
+  yarn lerna version --no-private --conventional-commits --conventional-prerelease --include-merged-tags --no-git-tag-version --yes
 else
   echo "No changed packages detected. Skipping lerna version."
 fi
