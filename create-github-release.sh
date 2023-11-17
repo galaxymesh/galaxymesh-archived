7674f817e4b259e5cbcc68504318468525a52179
Antonette Caldwell <acald.nebula@gmail.com>
2023-11-16 19:08:49 -0600

    chore(repo): revise permission on create-github-release.sh

---
+++ create-github-release.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Usage: create-github-release.sh
+# Example: create-github-release.sh
+
+TAGS=($(git tag -l --sort=-version:refname))
+
+for TAG in "${TAGS[@]}"; do
+  PACKAGE_NAME=$(grep -oP '(?<=name": ").*?(?=",)' packages/${TAG/\@/\/}/package.json)
+  gh release create $TAG ./packages/$PACKAGE_NAME -t "$TAG" -y
+done
