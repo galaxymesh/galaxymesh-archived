ecf7498b38974d13a1d9b9db72908aa8941f9265
Antonette Caldwell <134739862+nebula-aac@users.noreply.github.com>
2023-11-17 09:04:38 -0600

    feat(base): create Dialog components (#47)
    Create Dialog components and Backdrop/Slide components
    
    fix #45

---
+++ packages/galaxymesh-base/src/components/DialogContentText/index.tsx
@@ -0,0 +1,9 @@
+import { DialogContentText as MuiDialogContentText, DialogContentTextProps as MuiDialogContentTextProps } from '@mui/material';
+
+export function DialogContentText(props: MuiDialogContentTextProps): JSX.Element {
+    return (
+        <MuiDialogContentText {...props} />
+    )
+}
+
+export default MuiDialogContentText
