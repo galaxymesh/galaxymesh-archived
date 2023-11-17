e11a604482fd1b37f2dca51d24375e2c61c3005e
Antonette Caldwell <acald.nebula@gmail.com>
2023-11-16 20:39:16 -0600

    feat(next-14): create next.js 14 ts app
    Create Next.js 14 TS app to test changes from libraries
    
    fix #42

---
+++ apps/next-14/pages/index.tsx
@@ -0,0 +1,7 @@
+import { ResponsiveDataTable } from "@galaxymesh/ui";
+
+export default function Home() {
+  return (
+    <ResponsiveDataTable data={[]} columns={[]} columnVisibility={undefined} />
+  )
+}
